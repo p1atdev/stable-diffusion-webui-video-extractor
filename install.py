1049f5a105057614d9d34f474604ce9910174768
Plat <git@p1at.dev>
2023-03-27 19:07:05 +0900

    fix: git submodule update

--- install.py
+++ install.py
@@ -15,7 +15,7 @@ tagger_dir = extension_path / "tagger"
 if not tagger_dir.exists():
     # git submodule init
     launch.run(
-        f"cd {extension_path.name} & git submodule init",
+        f"cd {extension_path.name} && git submodule update --init --recursive",
         "init submodule for Video Extractor",
     )
 
